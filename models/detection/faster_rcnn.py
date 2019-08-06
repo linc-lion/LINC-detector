18a5e5854c26a162dde9752b341d2303a863426d
joaqo <joaquin.alori@gmail.com>
2019-08-06 20:15:32 -0300

    Reduce nms threshold a bit

--- models/detection/faster_rcnn.py
+++ models/detection/faster_rcnn.py
@@ -152,7 +152,7 @@ class FasterRCNN(GeneralizedRCNN):
                  rpn_batch_size_per_image=256, rpn_positive_fraction=0.5,
                  # Box parameters
                  box_roi_pool=None, box_head=None, box_predictor=None,
-                 box_score_thresh=0.05, box_nms_thresh=0.5, box_detections_per_img=100,
+                 box_score_thresh=0.05, box_nms_thresh=0.35, box_detections_per_img=100,
                  box_fg_iou_thresh=0.5, box_bg_iou_thresh=0.5,
                  box_batch_size_per_image=512, box_positive_fraction=0.25,
                  bbox_reg_weights=None):
