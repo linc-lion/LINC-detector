1886de99ccd337715b4488eb34b0b724593660da
shihgianlee <shih.lee@gmail.com>
2022-09-30 13:07:16 -0700

    Fixes bug.

--- linc/detector/helper/utils.py
+++ linc/detector/helper/utils.py
@@ -147,7 +147,7 @@ def reduce_dict(input_dict, average=True):
 
 
 class MetricLogger(object):
-    def __init__(self, delimiter="\t", device='cuda'):
+    def __init__(self, delimiter="\t", device=torch.device("cuda")):
         self.meters = defaultdict(SmoothedValue)
         self.delimiter = delimiter
         self.device = device
@@ -208,7 +208,7 @@ class MetricLogger(object):
             if i % print_freq == 0 or i == len(iterable) - 1:
                 eta_seconds = iter_time.global_avg * (len(iterable) - i)
                 eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
-                memory = torch.cuda.max_memory_allocated() if self.device == 'cuda' else 0.0
+                memory = torch.cuda.max_memory_allocated() if self.device.type == 'cuda' else 0.0
                 print(log_msg.format(
                     i, len(iterable), eta=eta_string,
                     meters=str(self),
