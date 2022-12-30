f6c52ccaada902aff085d93b0e8a618b5bdf37ea
shihgianlee <shih.lee@gmail.com>
2022-12-30 10:01:30 -0800

    Enhances setup.py to publish to public pypi server.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
-requirements_file = open('requirements_predict.txt')
-requirements = requirements_file.read().strip().split('\n')
-
-print(f'Setup requirements: {requirements}')
-
 setup(
     name='linc-detector',
-    description='Python package that holds model to detect lion parts',
-    install_requires=requirements,
-    version='0.2dev',
+    description='Python package that holds model to detect lion facial parts',
+    version='0.1',
     packages=find_packages(),
-    author='LINC',
-    author_email=' tech@linclion.org'
+    author='Lion Guardians',
+    author_email=' tech@linclion.org',
+    long_description='See https://github.com/linc-lion/LINC-detector',
+    long_description_content_type='text/x-rst',
+    install_requires=[
+        'numpy==1.23.3',
+        'future==0.17.1',
+        'torch==1.10.1',
+        'torchvision==0.11.2'
+    ],
+    classifiers=[
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
 )
